d876c3629a3fb168d0a0942e046816bd975cc7c0
Pawel Drapiewski <drapek39@gmail.com>
2021-02-10 22:51:30 +0100

    Upgrade User tests - check if admin prop is set

--- wedding/core/tests/test_models.py
+++ wedding/core/tests/test_models.py
@@ -13,8 +13,10 @@ class UserTestCase(TestCase):
         data = {**self.required_user_attrs, 'user_type': models.User.UserTypes.ADMIN}
         user = models.User.objects.create_user(**data)
         self.assertTrue(user.is_staff)
+        self.assertTrue(user.admin)
 
     def test_is_staff_flag_set_on_newlywed_user_type(self):
         data = {**self.required_user_attrs, 'user_type': models.User.UserTypes.NEWLYWED}
         user = models.User.objects.create_user(**data)
         self.assertTrue(user.is_staff)
+        self.assertTrue(user.admin)
